21f9a79e701ac5a2b27bbefe642d64ca478d86ee
Nishith <Nishith0514@gmail.com>
2024-07-18 22:47:03 -0400

    readiness route setup

--- src/main/java/AdvCloud/webapp/controllers/HealthController.java
+++ src/main/java/AdvCloud/webapp/controllers/HealthController.java
@@ -6,15 +6,32 @@ import AdvCloud.webapp.model.*;
 
 
 @RestController
-@RequestMapping("/v1/health-check")
+@RequestMapping("/v1")
 public class HealthController {
 
-    @GetMapping
+    @GetMapping("/health-check")
     public ResponseEntity<HealthStatus> getHealthCheck(){
         HealthStatus status = new HealthStatus();
         status.setStatus(200);
         status.setMessage("Success");
         return new ResponseEntity<>(status, HttpStatus.OK);
+    }
+
+    @GetMapping("/health-check-ready")
+    public ResponseEntity<HealthStatus> getHealthReadinessCheck(){
+        HealthStatus status = new HealthStatus();
+        boolean isReady = true;
+        if(isReady){
+            status.setStatus(200);
+            status.setMessage("Success");
+            return new ResponseEntity<>(status, HttpStatus.OK);
+        }
+        else{
+            status.setStatus(500);
+            status.setMessage("NotReady");
+            return new ResponseEntity<>(status, HttpStatus.SERVICE_UNAVAILABLE);
+        }
+        
     }
 
 }
